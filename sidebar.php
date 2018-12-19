af6ef90fe7da84d9143d1108f978d12a7bd0c172
zcoder <dusrksl@gmail.com>
2018-12-19 23:22:12 +0900

    2018/12/19 version 1.0

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="model-house">
+<div class="side-model">
     <p class="en-title">MODEL</p>
     <h2 class="heading">モデルハウス</h2>
     <div class="model-item">
@@ -33,7 +33,7 @@
     </div>
         
 </div>
-<div class="town-project">
+<div class="side-town">
     <p class="en-title">TOWN</p>
     <h2 class="heading">タウンプロジェクト</h2>
     <?php
@@ -46,20 +46,29 @@
                );
              ?>
             <?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-            <a class="article-town clearfix" href="<?php the_permalink(); ?>">
+            <a class="article-project clearfix" href="<?php the_permalink(); ?>">
                 <?php the_post_thumbnail('thumbnail', array('class' => 'image')); ?>
-                <h3 class="title"><?php the_title(); ?></h3>
-                <?php
-                  $content = get_the_content();
-                  $content = preg_replace("/<img[^>]+\>/i", " ", $content);
-                  $content = apply_filters('the_content', $content);
-                  $content = str_replace(']]>', ']]>', $content);
-                  echo $content;
-                ?>
+                <div class="project-item">
+                    <h3 class="title"><?php the_title(); ?></h3>
+                    <?php
+                    $content = get_the_content();
+                    $content = preg_replace("/<img[^>]+\>/i", " ", $content);
+                    $content = apply_filters('the_content', $content);
+                    $content = str_replace(']]>', ']]>', $content);
+                    echo $content;
+                    ?>
+                </div>
             </a>
             <?php endwhile; endif; wp_reset_postdata(); ?>
+            
 </div>
-<div class="oneon-project">
+<div class="more">
+                <a class="more-btn" href="">
+                <span class="more-text">MORE</span>
+                <span class="more-dir">></span>
+                </a>
+            </div>
+<div class="side-oneon">
     <p class="en-title">ONEON</p>    
     <h2 class="heading">ワンオンプロジェクト</h2>
     <?php
@@ -72,16 +81,59 @@
                );
              ?>
             <?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-            <a class="" href="<?php the_permalink(); ?>">
+            <a class="article-project clearfix" href="<?php the_permalink(); ?>">
                 <?php the_post_thumbnail('thumbnail', array('class' => 'image')); ?>
+                <div class="project-item">
                 <h3 class="title"><?php the_title(); ?></h3>
-                <?php
-                  $content = get_the_content();
-                  $content = preg_replace("/<img[^>]+\>/i", " ", $content);
-                  $content = apply_filters('the_content', $content);
-                  $content = str_replace(']]>', ']]>', $content);
-                  echo $content;
-                ?>
+                    <?php
+                    $content = get_the_content();
+                    $content = preg_replace("/<img[^>]+\>/i", " ", $content);
+                    $content = apply_filters('the_content', $content);
+                    $content = str_replace(']]>', ']]>', $content);
+                    echo $content;
+                    ?>
+                </div>
             </a>
             <?php endwhile; endif; wp_reset_postdata(); ?>
+            
+</div>
+<div class="more">
+    <a class="more-btn" href="">
+    <span class="more-text">MORE</span>
+    <span class="more-dir">></span>
+    </a>
+</div>
+<div class="side-newevent">
+    <p class="en-title">BLOG</p>    
+    <h2 class="heading">最近の出来事</h2>
+    <?php
+        $query = new WP_Query(
+        array(
+        'category__in' => array(6),
+        'posts_per_page' => 4,
+        'post_type' => 'post',
+        )
+        );
+    ?>
+    <?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
+    <a class="article-project clearfix" href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail('thumbnail', array('class' => 'image')); ?>
+        <div class="project-item">
+        <h3 class="title"><?php the_title(); ?></h3>
+            <?php
+            $content = get_the_content();
+            $content = preg_replace("/<img[^>]+\>/i", " ", $content);
+            $content = apply_filters('the_content', $content);
+            $content = str_replace(']]>', ']]>', $content);
+            echo $content;
+            ?>
+        </div>
+    </a>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+</div>
+<div class="more">
+    <a class="more-btn" href="">
+    <span class="more-text">MORE</span>
+    <span class="more-dir">></span>
+    </a>
 </div>
