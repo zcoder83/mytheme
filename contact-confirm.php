4914f5f32e5771ddb9d697d95762f90e335c901e
zcoder <dusrksl@gmail.com>
2018-12-26 23:43:49 +0900

    20181226

---
+++ contact-confirm.php
@@ -0,0 +1,5 @@
+<?php
+ /* Template Name: Contact Confirmation */ 
+ get_header(); ?>
+<?php echo do_shortcode( '[mwform_formkey key="96"]' ); ?>  
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -6,8 +6,9 @@
                 <?php if (have_posts()) : ?>
                     <?php while (have_posts()) : the_post();
                     /* ループ開始 */ ?>
+                       
+                       <?php the_category('>', 'multiple'); the_title(); ?>
                         <h3><?php the_title(); ?></h3>
-                        <p class="post-date"><?php the_time("Y年m月j日") ?></p>
                         <?php the_content(); ?>
 
                     <?php endwhile; ?>
@@ -17,7 +18,8 @@
                         <p>表示する記事はありませんでした。</p>
 
                 <?php endif; ?>
-            </div>    
+            </div> 
+            <?php get_template_part( "content" ); ?>
         </div>
         <div class="sidemenu">
             <?php get_sidebar(); ?>
