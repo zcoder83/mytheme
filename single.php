4205971129c5808c3e3d175d0511b53fa86234d7
zcoder <dusrksl@gmail.com>
2019-01-07 23:44:18 +0900

    2019.01.07 version2.0

--- single.php
+++ single.php
@@ -106,6 +106,48 @@
                             </div>
                             <?php endif; ?>   
                     <?php endwhile; ?>
+                    <a href="">この物件についてお問い合わせする</a>
+                    <div>
+                    <?php $orig_post = $post;
+                        global $post;
+                        $categories = get_the_category($post->ID);
+                        if ($categories) {
+                        $category_ids = array();
+                        foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+
+                        $args=array(
+                        'category__in' => $category_ids,
+                        'post__not_in' => array($post->ID),
+                        'posts_per_page'=> 3, // Number of related posts that will be shown.
+                        'caller_get_posts'=>1
+                        );
+
+                        $my_query = new wp_query( $args );
+                        if( $my_query->have_posts() ) {
+                        echo '<div id="related_posts"><h3>Related Posts</h3><ul>';
+                        while( $my_query->have_posts() ) {
+                        $my_query->the_post();?>
+
+                        <li><div class="relatedthumb"><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium', array('class' => 'related-image')); ?></a></div>
+                        <div class="relatedcontent">
+                        <h3><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+                        <?php
+                                        $content = get_the_content();
+                                        $content = preg_replace("/<img[^>]+\>/i", " ", $content);
+                                        $content = apply_filters('the_content', $content);
+                                        $content = str_replace(']]>', ']]>', $content);
+                                        echo $content;
+                                        ?>
+                        </div>
+                        </li>
+                        <?php
+                        }
+                        echo '</ul></div>';
+                        }
+                        }
+                        $post = $orig_post;
+                        wp_reset_query(); ?>
+                    </div>
                 <?php else : ?>
                         <h3>記事がありません</h3>
                         <p>表示する記事はありませんでした。</p>
